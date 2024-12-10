9f9e9d73df697925302702b2a1b68b8b7a4a39ac
JTDMedia <oudegraftak@proton.me>
2024-12-10 15:40:22 +0100

    Update deploy-commands.js
    Fixed my stupidity of inserting half of the code (it was the last half...)

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,13 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
 	const commandsPath = path.join(foldersPath, folder);
